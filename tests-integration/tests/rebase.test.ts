395e1cc26f1eefb0208f8a9fefdace3f2b91e17b
Christoph Dittmann <code@yozora.eu>
2026-03-22 20:29:36 +0100

    test: Use Playwright's dragTo() in rebase test

--- tests-integration/tests/rebase.test.ts
+++ tests-integration/tests/rebase.test.ts
@@ -14,63 +14,7 @@ test("rebase commit onto another via drag and drop", async ({ graphFrame, testRe
   const commitC = nodes.nth(1);
   const commitA = nodes.nth(3);
 
-  const cChangeId = await commitC.getAttribute("data-change-id");
-  const aChangeId = await commitA.getAttribute("data-change-id");
-
-  if (!cChangeId || !aChangeId) {
-    throw new Error("Could not get change IDs");
-  }
-
-  await graphFrame.evaluate(
-    ({ cChangeId, aChangeId }) => {
-      const cNode = document.querySelector(`.change-node[data-change-id="${cChangeId}"]`) as HTMLElement;
-      const aNode = document.querySelector(`.change-node[data-change-id="${aChangeId}"]`) as HTMLElement;
-
-      if (!cNode || !aNode) {
-        throw new Error("Could not find nodes");
-      }
-
-      const cRect = cNode.getBoundingClientRect();
-      const aRect = aNode.getBoundingClientRect();
-
-      const mousedownEvent = new MouseEvent("mousedown", {
-        bubbles: true,
-        cancelable: true,
-        clientX: cRect.left + cRect.width / 2,
-        clientY: cRect.top + cRect.height / 2,
-        button: 0,
-      });
-      cNode.dispatchEvent(mousedownEvent);
-
-      const intermediateX = cRect.left + cRect.width / 2 + 10;
-      const intermediateY = cRect.top + cRect.height / 2 + 10;
-      const mousemoveEvent1 = new MouseEvent("mousemove", {
-        bubbles: true,
-        cancelable: true,
-        clientX: intermediateX,
-        clientY: intermediateY,
-      });
-      document.dispatchEvent(mousemoveEvent1);
-
-      const mousemoveEvent2 = new MouseEvent("mousemove", {
-        bubbles: true,
-        cancelable: true,
-        clientX: aRect.left + aRect.width / 2,
-        clientY: aRect.top + aRect.height / 2,
-      });
-      document.dispatchEvent(mousemoveEvent2);
-
-      const mouseupEvent = new MouseEvent("mouseup", {
-        bubbles: true,
-        cancelable: true,
-        clientX: aRect.left + aRect.width / 2,
-        clientY: aRect.top + aRect.height / 2,
-        button: 0,
-      });
-      document.dispatchEvent(mouseupEvent);
-    },
-    { cChangeId, aChangeId },
-  );
+  await commitC.dragTo(commitA);
 
   const rebaseOntoItem = graphFrame.locator('.context-menu-item[data-action="rebaseOnto"]');
   await expect(rebaseOntoItem).toBeVisible({ timeout: 5000 });
