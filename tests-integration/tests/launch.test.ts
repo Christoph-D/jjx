4407f97ebfdb9b751b1c38b0c9ff6a40ae38cf32
Christoph Dittmann <code@yozora.eu>
2026-03-22 17:43:40 +0100

    test: Remove unnecessary launch test

--- tests-integration/tests/launch.test.ts
+++
@@ -1,5 +0,0 @@
-import { test, expect } from "./baseTest";
-
-test("VS Code launches with jjx extension", async ({ workbox }) => {
-  await expect(workbox.locator(".monaco-workbench")).toBeVisible({ timeout: 30000 });
-});
