fbba7ea675ed6cdfdcff2e0ba5b9bb7fe432a4d9
Christoph Dittmann <code@yozora.eu>
2026-08-14 23:20:52 +0200

    test: Reduce test timeout and retries when not on CI

--- tests-integration/playwright.config.ts
+++ tests-integration/playwright.config.ts
@@ -6,15 +6,15 @@ export type TestOptions = {
 
 export default defineConfig<void, TestOptions>({
   reporter: process.env.CI ? [["list"], ["blob", { outputDir: "blob-report" }]] : "list",
-  timeout: 60_000,
-  retries: 2,
+  timeout: process.env.CI ? 60_000 : 30_000,
+  retries: process.env.CI ? 2 : 0,
   workers: process.platform === "darwin" ? 4 : 5,
   fullyParallel: true,
   use: {
     screenshot: "only-on-failure",
   },
   expect: {
-    timeout: 30_000,
+    timeout: process.env.CI ? 30_000 : 15_000,
   },
   globalSetup: "./global-setup",
   projects: [
